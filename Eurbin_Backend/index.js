95c8315a0790ac33f1a400d03d02e87651c886c2
JQuerel <bilmokooo@gmail.com>
2024-09-17 11:39:04 +0800

    09/17/24

--- Eurbin_Backend/index.js
+++ Eurbin_Backend/index.js
@@ -1,26 +1,24 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
 app.use(cors());
 
-const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 
-
-
-const transactionRoutes = require('./routes/transactionRoutes')
+const transactionRoutes = require('./routes/transactionRoutes');
 const rewardRoutes = require('./routes/rewardRoutes');
 const plasticBottleRoutes = require('./routes/plasticBottleRoutes');
 const authRoutes = require('./routes/authRoutes'); 
-const redeemCodeRoutes = require('./routes/redeemCodeRoutes'); 
-const userRoutes = require('./routes/userRoutes'); 
-const totalRoutes =  require('./routes/totalRoutes'); 
-
+const redeemCodeRoutes = require('./routes/redeemCodeRoutes');
+const userRoutes = require('./routes/userRoutes');
+const totalRoutes =  require('./routes/totalRoutes');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-const Connection_String = "mongodb+srv://JQuerel:JQuerel@eurbin.th0jg.mongodb.net/EURBin?retryWrites=true&w=majority";
+const Connection_String = process.env.MONGODB_URI;
 
 mongoose.connect(Connection_String)
     .then(() => {
@@ -30,26 +28,16 @@ mongoose.connect(Connection_String)
         console.error('Error connecting to MongoDB Atlas', err);
     });
 
-
 app.get('/', (req, res) => {
-    res.send('Welcome to the EURBin Backend! This server manages user transactions, rewards, plastic bottle identification, and code redemption processes. Stay tuned for more updates.');
+    res.send('Welcome to the EURBin Backend! This server manages user transactions, rewards, plastic bottle identification, and code redemption processes.');
 });
 
 app.use('/rewards', rewardRoutes);
-
 app.use('/transactions', transactionRoutes);
-
 app.use('/bottles', plasticBottleRoutes);
+app.use('/admin', authRoutes);
+app.use('/code', redeemCodeRoutes);
+app.use('/user', userRoutes);
+app.use('/total', totalRoutes);
 
-app.use('/admin', authRoutes); 
-
-app.use('/code', redeemCodeRoutes); 
-
-app.use('/user', userRoutes); 
-
-app.use('/total', totalRoutes); 
-
-const port = 7000;
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+module.exports = app;
