89701a98e672062875e68aa2f3d92718e1b06b0a
JQuerel <bilmokooo@gmail.com>
2024-09-19 10:44:10 +0800

    09/19/24

--- Eurbin_Backend/api/index.js
+++ Eurbin_Backend/api/index.js
@@ -13,7 +13,7 @@ const userRoutes = require('../routes/userRoutes');
 const totalRoutes = require('../routes/totalRoutes');
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(bodyParser.json());
 
 // MongoDB connection
