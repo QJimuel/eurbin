99ee81a6adb0cd99ca517406ea082c9d549d5263
JQuerel <bilmokooo@gmail.com>
2024-09-20 20:26:57 +0800

    09/20/24

--- Eurbin_Backend/api/index.js
+++ Eurbin_Backend/api/index.js
@@ -17,7 +17,7 @@ const app = express();
 const corsOptions = {
     origin: 'http://localhost:5173',
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
