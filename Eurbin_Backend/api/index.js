a93596eaaa818dfcf17a2ef983e53f91028dbab3
JQuerel <bilmokooo@gmail.com>
2024-09-20 20:17:01 +0800

    09/20/24

--- Eurbin_Backend/api/index.js
+++ Eurbin_Backend/api/index.js
@@ -22,6 +22,15 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+
+app.options('*', (req, res) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.sendStatus(200); // Send OK response
+});
+
 app.use(bodyParser.json());
 
 // MongoDB connection
