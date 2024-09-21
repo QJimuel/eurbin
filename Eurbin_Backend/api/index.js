6ea8a1ed49c61232023556ed856275f27d0181a1
JQuerel <bilmokooo@gmail.com>
2024-09-21 17:47:41 +0800

    09/21/24

--- Eurbin_Backend/api/index.js
+++ Eurbin_Backend/api/index.js
@@ -16,7 +16,7 @@ const contentRoutes = require('../routes/contentRoutes')
 const app = express();
 
 const corsOptions = {
-    origin: 'https://localhost:5173',
+    origin: 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
@@ -25,7 +25,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.options('*', (req, res) => {
-    res.header('Access-Control-Allow-Origin', 'https://localhost:5173');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
