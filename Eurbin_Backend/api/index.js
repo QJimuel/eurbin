802d9a815982665cad576c8b2558ae4b04474889
JQuerel <bilmokooo@gmail.com>
2024-09-27 09:55:44 +0800

    Allow React Native App CORS for fetching data

--- Eurbin_Backend/api/index.js
+++ Eurbin_Backend/api/index.js
@@ -16,7 +16,7 @@ const contentRoutes = require('../routes/contentRoutes')
 const app = express();
 
 const corsOptions = {
-    origin: ['https://eurbinadmin.vercel.app', 'http://localhost:5173'],
+    origin:'*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
@@ -26,7 +26,7 @@ app.use(cors(corsOptions));
 
 
 app.options('*', (req, res) => {
-    res.header('Access-Control-Allow-Origin', ['https://eurbinadmin.vercel.app','http://localhost:5173','http://localhost:8081' ]);
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
