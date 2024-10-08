a66d9caf399549f321fd940c8bd5d026475655e7
JQuerel <bilmokooo@gmail.com>
2024-10-08 16:03:00 +0800

    Try adding user auth

--- Eurbin_Backend/routes/userRoutes.js
+++ Eurbin_Backend/routes/userRoutes.js
@@ -11,10 +11,10 @@ router.post('/', userController.createUser);
 router.post('/login', userController.loginUser);
 
 // Protected Routes
-router.get('/',  userController.getAllUsers);
-router.get('/:id',  userController.getUserById);
-router.put('/:id',  userController.updateUser);
-router.delete('/:id',  userController.deleteUser);
-router.put('/update-password',  userController.updatePassword);
+router.get('/',authenticateToken,  userController.getAllUsers);
+router.get('/:id',authenticateToken,  userController.getUserById);
+router.put('/:id', authenticateToken, userController.updateUser);
+router.delete('/:id', authenticateToken, userController.deleteUser);
+router.put('/update-password', authenticateToken,   userController.updatePassword);
 
 module.exports = router;
