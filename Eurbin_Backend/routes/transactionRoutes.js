48d083a4d523534a18d127d1466d59ca6c909b80
JQuerel <bilmokooo@gmail.com>
2024-11-13 22:01:13 +0800

    Eurbin Backend: Admin Auth for Transaction Management

--- Eurbin_Backend/routes/transactionRoutes.js
+++ Eurbin_Backend/routes/transactionRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const transactionController = require('../controllers/transactionController');
+const adminAuthMiddleware = require('../middlewares/adminMiddleware');
 
 // Existing routes
 router.get('/', transactionController.getAllTransactions);
 router.post('/', transactionController.createTransaction);
 
 // New route to update transaction status
-router.put('/:transactionId', transactionController.updateTransactionStatus);
+router.put('/:transactionId', adminAuthMiddleware,transactionController.updateTransactionStatus);
 
 module.exports = router;
 
