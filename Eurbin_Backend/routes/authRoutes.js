c044ce5ac91a1c4992a8c4a4f9567bf05902034f
JQuerel <bilmokooo@gmail.com>
2024-11-17 03:42:26 +0800

    Eurbin Backend: Admin  Auth for Content, Plastic Bottle, Codes, and total

--- Eurbin_Backend/routes/authRoutes.js
+++ Eurbin_Backend/routes/authRoutes.js
@@ -2,13 +2,14 @@
 const express = require('express');
 const adminController = require('../controllers/adminController');
 const adminAuthMiddleware = require('../middlewares/adminMiddleware');
+const authenticateToken = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.get('/',adminAuthMiddleware,adminController.getAllAdmins)
 router.post('/register', adminController.register);
 router.post('/login', adminController.login);
-router.patch('/update-profile', adminController.updateProfile); 
-router.patch('/change-password', adminController.changePassword);
+router.patch('/update-profile',authenticateToken, adminController.updateProfile); 
+router.patch('/change-password',authenticateToken, adminController.changePassword);
 router.get('/verify-email', adminController.verifyEmail);
 // Add this route
 
--- Eurbin_Backend/routes/redeemCodeRoutes.js
+++ Eurbin_Backend/routes/redeemCodeRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const redeemCodeController = require('../controllers/redeemCodeController');
 const adminAuthMiddleware = require('../middlewares/adminMiddleware'); 
+const authenticateToken = require('../middlewares/authMiddleware');
 
 router.get('/',adminAuthMiddleware, redeemCodeController.getAllCode);
 router.post('/', redeemCodeController.createCode);
-router.put('/', redeemCodeController.redeemCode)
+router.put('/',authenticateToken, redeemCodeController.redeemCode)
 
 module.exports = router;
