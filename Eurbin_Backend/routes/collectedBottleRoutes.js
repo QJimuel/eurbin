08531bcb8297b4ec6535e692d5f1de7d48f4cacf
JQuerel <bilmokooo@gmail.com>
2024-11-26 11:04:52 +0800

    Eurbin-admin

--- Eurbin_Backend/routes/collectedBottleRoutes.js
+++ Eurbin_Backend/routes/collectedBottleRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const collectedBottleController = require('../controllers/collectedBottleController');
 const adminAuthMiddleware = require('../middlewares/adminMiddleware'); 
+const authenticateToken = require('../middlewares/authMiddleware');
 
 
-router.get('/',adminAuthMiddleware, collectedBottleController.getAllCollectedBottles);
+router.get('/',authenticateToken, collectedBottleController.getAllCollectedBottles);
 router.post('/',adminAuthMiddleware, collectedBottleController.createCollectedBottle);
 
 module.exports = router;
